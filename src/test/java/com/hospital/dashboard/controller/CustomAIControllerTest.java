6cf96d22f2e7f7c3ca25f01a6da2157ccc315969
KriTeRiuM0 <saboualam3@gmail.com>
2025-12-21 18:00:21 +0100

    Fix compilation error: Add missing any() import

--- src/test/java/com/hospital/dashboard/controller/CustomAIControllerTest.java
+++ src/test/java/com/hospital/dashboard/controller/CustomAIControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
